9dc87886e7b9b9ed60f9f5afc7eefaefb88a1d20
李铭昕 <715557344@qq.com>
2023-08-11 15:43:45 +0800

    fix openssl

--- Formula/php@8.1.rb
+++ Formula/php@8.1.rb
@@ -187,10 +187,10 @@ class PhpAT81 < Formula
       args << "--without-gdbm"
     end
 
-    system "export", "PATH=#{Formula["openssl@1.1"].opt_prefix}/bin:$PATH"
-    system "export", "LDFLAGS=-L #{Formula["openssl@1.1"].opt_prefix}/lib"
-    system "export", "CPPFLAGS=-I #{Formula["openssl@1.1"].opt_prefix}/include"
-    system "export", "PKG_CONFIG_PATH=#{Formula["openssl@1.1"].opt_prefix}/lib/pkgconfig"
+    system "export PATH=#{Formula["openssl@1.1"].opt_prefix}/bin:$PATH"
+    system "export LDFLAGS=-L#{Formula["openssl@1.1"].opt_prefix}/lib"
+    system "export CPPFLAGS=-I#{Formula["openssl@1.1"].opt_prefix}/include"
+    system "export PKG_CONFIG_PATH=#{Formula["openssl@1.1"].opt_prefix}/lib/pkgconfig:$PKG_CONFIG_PATH"
     
     system "./configure", *args
     system "make"
